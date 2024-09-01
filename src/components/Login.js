e0c1ab1f94c42a30ac1d5fd587ee89079a77e0a5
rangguy <ranggadwi100@gmail.com>
2024-09-01 22:21:15 +0700

    setting up User Login

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,8 +1,61 @@
+import { useState } from "react";
+import Input from "./form/Input";
+import { useNavigate, useOutletContext } from "react-router-dom";
+
 const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const { setJwtToken, setAlertMessage, setAlertClassName } =
+    useOutletContext();
+
+  const navigate = useNavigate();
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log("email/pass", email, password);
+
+    if (email === "admin@mail.com") {
+      setJwtToken("abc");
+      setAlertMessage("");
+      setAlertClassName("d-none");
+      navigate("/");
+    } else {
+      setAlertClassName("alert-danger");
+      setAlertMessage("Invalid Credentials");
+    }
+  };
+
   return (
-    <div>
+    <div className="col-md-6 offset-md-3">
       <h2>Login</h2>
       <hr />
+
+      <form onSubmit={handleSubmit}>
+        <Input
+          title="Email Address"
+          type="email"
+          className="form-control"
+          name="email"
+          id="email"
+          autoComplete="email-new"
+          onChange={(event) => setEmail(event.target.value)}
+        />
+
+        <Input
+          title="Password"
+          type="password"
+          className="form-control"
+          name="password"
+          id="password"
+          autoComplete="password-new"
+          onChange={(event) => setPassword(event.target.value)}
+        />
+
+        <hr />
+
+        <Input type="submit" className="btn btn-primary" value="Login" />
+      </form>
     </div>
   );
 };
