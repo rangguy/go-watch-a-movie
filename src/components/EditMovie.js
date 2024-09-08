71cbdc7bd5f802d007cc179279e4af149099f392
rangguy <ranggadwi100@gmail.com>
2024-09-08 22:48:41 +0700

    validation add/edit movie

--- src/components/EditMovie.js
+++ src/components/EditMovie.js
@@ -1,8 +1,253 @@
+import { useEffect, useState } from "react";
+import { useNavigate, useOutletContext, useParams } from "react-router-dom";
+import Input from "./form/Input";
+import Select from "./form/Select";
+import TextArea from "./form/TextArea";
+import Checkbox from "./form/Checkbox";
+import Swal from "sweetalert2";
+
 const EditMovie = () => {
+  const navigate = useNavigate();
+  const { jwtToken } = useOutletContext();
+
+  // for individual error
+  const [error, setError] = useState(null);
+  // provide all errors
+  const [errors, setErrors] = useState([]);
+
+  const mpaaOptions = [
+    { id: "G", value: "G" },
+    { id: "PG", value: "PG" },
+    { id: "PG13", value: "PG13" },
+    { id: "R", value: "R" },
+    { id: "NC17", value: "NC17" },
+    { id: "18A", value: "18A" },
+  ];
+
+  const hasError = (key) => {
+    return errors.indexOf(key) !== -1;
+  };
+
+  const [movie, setMovie] = useState({
+    id: 0,
+    title: "",
+    release_date: "",
+    runtime: "",
+    mpaa_rating: "",
+    description: "",
+    genres: [],
+    genres_array: [Array(13).fill(false)],
+  });
+
+  // get id from the url
+  let { id } = useParams();
+  if (id === undefined) {
+    id = 0;
+  }
+
+  useEffect(() => {
+    if (jwtToken === "") {
+      navigate("/login");
+      return;
+    }
+
+    if (id === 0) {
+      // adding the movie
+      setMovie({
+        id: 0,
+        title: "",
+        release_date: "",
+        runtime: "",
+        mpaa_rating: "",
+        description: "",
+        genres: [],
+        genres_array: [Array(13).fill(false)],
+      });
+
+      const headers = new Headers();
+      headers.append("Content-Type", "application/json");
+
+      const requestOptions = {
+        method: "GET",
+        headers: headers,
+      };
+
+      fetch(`/genres`, requestOptions)
+        .then((response) => response.json())
+        .then((data) => {
+          const checks = [];
+
+          data.forEach((g) => {
+            checks.push({ id: g.id, checked: false, genre: g.genre });
+          });
+
+          setMovie((m) => ({
+            ...m,
+            genres: checks,
+            genres_array: [],
+          }));
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    } else {
+      // editing an existing movie
+    }
+  }, [id, jwtToken, navigate]);
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    let errors = [];
+
+    let required = [
+      { field: movie.title, name: "title" },
+      { field: movie.release_date, name: "release_date" },
+      { field: movie.runtime, name: "runtime" },
+      { field: movie.description, name: "description" },
+      { field: movie.mpaa_rating, name: "mpaa_rating" },
+    ];
+
+    required.forEach(function (obj) {
+      if (obj.field === "") {
+        errors.push(obj.name);
+      }
+    });
+
+    if (movie.genres_array.length === 0) {
+      Swal.fire({
+        tite: "Error",
+        text: "You must choose at least one genre!",
+        icon: "error",
+        confirmButtonText: "OK",
+      });
+      errors.push("genres");
+    }
+
+    setErrors(errors);
+
+    if (errors.length > 0) {
+      return false;
+    }
+  };
+
+  const handleChange = () => (event) => {
+    let value = event.target.value;
+    let name = event.target.name;
+
+    setMovie({
+      ...movie,
+      [name]: value,
+    });
+  };
+
+  const handleCheck = (event, position) => {
+    console.log("handleCheck called");
+    console.log("value in handleCheck:", event.target.value);
+    console.log("checked is", event.target.checked);
+    console.log("position is", position);
+
+    let tmpArr = movie.genres;
+    tmpArr[position].checked = !tmpArr[position].checked;
+
+    let tmpIDs = movie.genres_array;
+    if (!event.target.checked) {
+      tmpIDs.splice(tmpIDs.indexOf(event.target.value));
+    } else {
+      tmpIDs.push(parseInt(event.target.value, 10));
+    }
+
+    setMovie({
+      ...movie,
+      genres_array: tmpIDs,
+    });
+  };
+
   return (
     <div>
-      <h2>Edit Movie</h2>
+      <h2>Add/Edit Movie</h2>
       <hr />
+      {/* <pre>{JSON.stringify(movie, null, 3)}</pre> */}
+
+      <form onSubmit={handleSubmit}>
+        <input type="hidden" name="id" value={movie.id} id="id"></input>
+        <Input
+          title={"Title"}
+          className={"form-control"}
+          type={"text"}
+          name={"title"}
+          value={movie.title}
+          onChange={handleChange("title")}
+          errorDiv={hasError("title") ? "text-danger" : "d-none"}
+          errorMsg={"Please enter a title"}
+        />
+
+        <Input
+          title={"Release Date"}
+          className={"form-control"}
+          type={"date"}
+          name={"release_date"}
+          value={movie.release_date}
+          onChange={handleChange("release_date")}
+          errorDiv={hasError("release_date") ? "text-danger" : "d-none"}
+          errorMsg={"Please enter a release date"}
+        />
+
+        <Input
+          title={"Runtime"}
+          className={"form-control"}
+          type={"text"}
+          name={"runtime"}
+          value={movie.runtime}
+          onChange={handleChange("runtime")}
+          errorDiv={hasError("runtime") ? "text-danger" : "d-none"}
+          errorMsg={"Please enter a runtime"}
+        />
+
+        <Select
+          title={"MPAA Rating"}
+          name={"mpaa_rating"}
+          options={mpaaOptions}
+          onChange={handleChange("mpaa_rating")}
+          placeHolder={"Choose..."}
+          errorMsg={"Please choose"}
+          errorDiv={hasError("mpaa_rating") ? "text-danger" : "d-none"}
+        />
+
+        <TextArea
+          title={"Description"}
+          name={"description"}
+          value={movie.description}
+          rows={"3"}
+          onChange={handleChange("description")}
+          errorDiv={hasError("description") ? "text-danger" : "d-none"}
+          errorMsg={"Please enter a description"}
+        />
+
+        <hr />
+
+        <h3>Genres</h3>
+
+        {movie.genres && movie.genres.length > 1 && (
+          <>
+            {Array.from(movie.genres).map((g, index) => (
+              <Checkbox
+                title={g.genre}
+                name={"genre"}
+                key={index}
+                id={"genre-" + index}
+                onChange={(event) => handleCheck(event, index)}
+                value={g.id}
+                checked={movie.genres[index].checked}
+              />
+            ))}
+          </>
+        )}
+
+        <hr />
+
+        <button className="btn btn-primary">Save</button>
+      </form>
     </div>
   );
 };
--- src/components/Movie.js
+++ src/components/Movie.js
@@ -6,24 +6,57 @@ const Movie = () => {
   let { id } = useParams();
 
   useEffect(() => {
-    let myMovie = {
-      id: 1,
-      title: "Queen of Tears",
-      release_date: "2024-05-08",
-      runTime: 62,
-      mpaa_rating: "R",
-      desciption:
-        "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quo illo, accusamus neque sint dicta mollitia rerum possimus, odio officia aperiam optio cumque. Repellendus eveniet omnis et cum voluptate, animi quos?",
+    const headers = new Headers();
+    headers.append("Content-Type", "application/json");
+
+    const requestOptions = {
+      method: "GET",
+      headers: headers,
     };
-    setMovie(myMovie);
+
+    fetch(`/movies/${id}`, requestOptions)
+      .then((response) => response.json())
+      .then((data) => {
+        setMovie(data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
   }, [id]);
+
+  if (movie.genres) {
+    movie.genres = Object.values(movie.genres);
+  } else {
+    movie.genres = [];
+  }
 
   return (
     <div>
       <h2>Movie: {movie.title}</h2>
-      <small><em>{movie.release_date}, {movie.runTime} minutes, Rated {movie.mpaa_rating}</em></small>
+      <small>
+        <em>
+          {movie.release_date}, {movie.runTime} minutes, Rated{" "}
+          {movie.mpaa_rating}
+        </em>
+      </small>
+      <br />
+      {movie.genres.map((g) => (
+        <span key={g.genre} className="badge bg-secondary me-2">
+          {g.genre}
+        </span>
+      ))}
       <hr />
-      <p>{movie.desciption}</p>
+
+      {movie.image !== "" && (
+        <div className="mb-3">
+          <img
+            src={`https://image.tmdb.org/t/p/w200/${movie.image}`}
+            alt="poster"
+          />
+        </div>
+      )}
+
+      <p>{movie.description}</p>
     </div>
   );
 };
